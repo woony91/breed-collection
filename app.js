46cffd326b60963f5cd8d166cb3deac455557878
woony91 <49602639+woony91@users.noreply.github.com>
2019-09-29 21:03:21 -0400

    updated to finalize breed assignment

--- app.js
+++ app.js
@@ -3,40 +3,29 @@
 function getDogImage(breed) {
   fetch(`https://dog.ceo/api/breed/${breed}/images/random`)
     .then(response => response.json())
-    .then(responseJson =>
+    .then(responseJson => 
       displayResults(responseJson))
     .catch(error => alert('Something went wrong. Try again later.'));
 }
 
 function displayResults(responseJson) {
   console.log(responseJson);
-
-  if (responseJson.message == "Breed not found") {
-    alert('That breed wasn\'t found, please try another.');
-  } else {
-    //replace the existing image with the new one
-    $('.results').html(`<h2>Look at this dog!</h2>`);
-
-    let splitUrl = responseJson.message.split("/");
-    let breedName = splitUrl[4];
-    $('.results').append(`<h3>${breedName}</h3>`);
-
-    $('.results').append(
-      `<img src="${responseJson.message}" class="results-img" width="200" height="auto">`);
-    //display the results section
-    $('.results').removeClass('hidden');
-  }
+  //replace the existing image with the new one
+  $('.results-img').replaceWith(
+    `<img src="${responseJson.message}" class="results-img">`
+  )
+  //display the results section
+  $('.results').removeClass('hidden');
 }
 
 function watchForm() {
   $('form').submit(event => {
     event.preventDefault();
-    let dogBreed = $('input[name="dogBreed"]').val();
-    getDogImage(dogBreed);
+    getDogImage($('#breed').val());
   });
 }
 
-$(function () {
+$(function() {
   console.log('App loaded! Waiting for submit!');
   watchForm();
 });
